19e9004e7143b4ca6fa061f1c1269323a058b289
AdetolaAremu <75201167+AdetolaAremu@users.noreply.github.com>
2021-03-07 04:04:11 -0800

    Add files via upload

---
+++ Appfooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="bg-green-700 h-14">
+    <p class="text-white">9pay copyright 2021</p>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Appheader.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="bg-green-600 h-16">
+    <div class="flex justify-between px-24 py-2">
+      <img src="../assets/images/92.jpg" class="w-20 h-12">
+      <div class="text-white font-semibold mt-3">
+        <a class="mr-3" href="#">Login</a>
+        <a class="mr-3" href="#">Sign Up</a>
+        <a class="mr-3" href="#">Explore</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
